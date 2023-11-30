c12fc891e66ccc07417de96d9bfdfd6a2896fd7e
Omar Mirza <omirza@gmail.com>
2023-11-30 12:16:49 -0500

    Bundle and display project array

--- app/src/components/Projects.jsx
+++ app/src/components/Projects.jsx
@@ -3,8 +3,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { /* reducers */ } from "../deckhandSlice";
 
 export default function Projects() {
-  const state = useSelector((state) => state.main);
+  const state = useSelector((state) => state.deckhand);
   const dispatch = useDispatch();
 
-  return <>Project list</>;
+  const projectBundle = [];
+  for (const el of state.projectArr) {
+    projectBundle.push(
+      <div key={el.id} class="card">
+        <div class="name">{el.name}</div>
+        <div class="date-info">
+          Created: {el.created_date}<br />
+          Last Modified: {el.modified_date}
+        </div>
+      </div>
+    )
+  }
+
+  return <>
+    <h1>Projects</h1>
+    <div id="project-cards">
+      {projectBundle}
+    </div>
+  </>;
 }
