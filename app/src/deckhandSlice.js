bc3007d2ab52c7ba4b3bc1832bcfdf964aebdeae
Omar Mirza <omirza@gmail.com>
2023-11-30 17:12:37 -0500

    Updated projectArr initial state

--- app/src/deckhandSlice.js
+++ app/src/deckhandSlice.js
@@ -8,7 +8,11 @@ export const deckhandSlice = createSlice({
 
         projectArr: [
             { id: 0, name: 'Example App', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-            { id: 1, name: 'Test Environment', created_date: 'Oct 15, 2023', modified_date: 'Nov 28, 2023' },
+            { id: 1, name: 'Example App', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+            { id: 2, name: 'Example App', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+            { id: 3, name: 'Example App', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+            { id: 4, name: 'Example App', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+            { id: 5, name: 'Example App', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
         ], // [ {id, name, created_date, modified_date, ...}, ... ]
 
         project: null, // id
