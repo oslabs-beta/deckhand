5e07fa34024f3d263e6326948152df8707c0039b
Omar Mirza <omirza@gmail.com>
2023-11-29 20:23:47 -0500

    Added state and reducers for projects

--- app/src/deckhandSlice.js
+++ app/src/deckhandSlice.js
@@ -4,10 +4,18 @@ export const deckhandSlice = createSlice({
     name: 'deckhand',
     initialState: {
         user: null, // {id, name, ...},
+        projectArr: null, // [ {id, name, created_date, modified_date, ...}, ... ]
+        project: null, // id
     },
     reducers: {
         setUser: (state, action) => {
             state.user = action.payload;
+        },
+        setProjectArr: (state, action) => {
+            state.projectArr = action.payload;
+        },
+        setProject: (state, action) => {
+            state.project = action.payload;
         },
     },
 });
@@ -15,6 +23,8 @@ export const deckhandSlice = createSlice({
 // Export actions for use in components
 export const {
     setUser,
+    setProjectArr,
+    setProject,
 } = deckhandSlice.actions;
 
 // Export the reducer function for store configuration
