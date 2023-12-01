9753930d716afbcee4c1c5ed389db4bcb5399e16
Omar Mirza <omirza@gmail.com>
2023-12-01 17:39:19 -0500

    Formatting

--- app/src/deckhandSlice.js
+++ app/src/deckhandSlice.js
@@ -1,40 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const deckhandSlice = createSlice({
-    name: 'deckhand',
-    initialState: {
-        user: null,
-        // user: { name: 'John' }, // {id, name, photo_url, ...},
+  name: 'deckhand',
+  initialState: {
+    user: null,
+    // user: { name: 'John' }, // {id, name, photo_url, ...},
 
-        projectArr: [
-            { id: 1, name: 'Example App 1', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-            { id: 2, name: 'Example App 2', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-            { id: 3, name: 'Example App 3', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-            { id: 4, name: 'Example App 4', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-            { id: 5, name: 'Example App 5', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-            { id: 6, name: 'Example App 6', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-        ], // [ {id, name, created_date, modified_date, ...}, ... ]
+    projectArr: [
+      { id: 1, name: 'Example App 1', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+      { id: 2, name: 'Example App 2', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+      { id: 3, name: 'Example App 3', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+      { id: 4, name: 'Example App 4', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+      { id: 5, name: 'Example App 5', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+      { id: 6, name: 'Example App 6', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
+    ], // [ {id, name, created_date, modified_date, ...}, ... ]
 
-        project: null, // id
+    project: null, // id
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
     },
-    reducers: {
-        setUser: (state, action) => {
-            state.user = action.payload;
-        },
-        setProjectArr: (state, action) => {
-            state.projectArr = action.payload;
-        },
-        setProject: (state, action) => {
-            state.project = action.payload;
-        },
+    setProjectArr: (state, action) => {
+      state.projectArr = action.payload;
     },
+    setProject: (state, action) => {
+      state.project = action.payload;
+    },
+  },
 });
 
 // Export actions for use in components
 export const {
-    setUser,
-    setProjectArr,
-    setProject,
+  setUser,
+  setProjectArr,
+  setProject,
 } = deckhandSlice.actions;
 
 // Export the reducer function for store configuration
--- server/server.js
+++ server/server.js
@@ -14,27 +14,27 @@ app.use('/build', express.static(path.join(__dirname, '../app/build')));
 
 // route handler to respond with main app
 app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.join(__dirname, '../app/public/index.html'));
+  return res.status(200).sendFile(path.join(__dirname, '../app/public/index.html'));
 });
 
 // catch-all route handler for any requests to an unknown route
 app.use('*', (req, res) => {
-    return res.sendStatus(404);
+  return res.sendStatus(404);
 });
 
 // global error handling middleware
 app.use((err, req, res, next) => {
-    const defaultErr = {
-        log: 'Express error handler caught unknown middleware error',
-        status: 500,
-        message: { err: 'An error occurred' },
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message);
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 // initialize port listening
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}...`);
+  console.log(`Server listening on port: ${PORT}...`);
 });
