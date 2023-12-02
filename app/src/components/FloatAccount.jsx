43c7131b487f2aa8c9ec58a74dd0d5f2b9ac227a
Omar Mirza <omirza@gmail.com>
2023-12-02 15:31:26 -0500

    Updated floating account

--- app/src/components/FloatAccount.jsx
+++ app/src/components/FloatAccount.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { /* reducers */ } from "../deckhandSlice";
-import account from '../assets/account.png';
+// import { /* reducers */ } "../deckhandSlice";
+import account from "../assets/account.png";
 
 export default function Deckhand() {
   const state = useSelector((state) => state.deckhand);
   const dispatch = useDispatch();
 
-  return <div className="account">
-    <span>{state.user.name}</span>
-    <img src={account} alt="Account" />
-  </div>
+  return (
+    <div className="account">
+      <span>{state.user.name}</span>
+      <img src={account} alt="Account" />
+    </div>
+  );
 }
