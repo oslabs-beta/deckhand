b1cb1393ed2b6ebb219c6e9f933fbd290cf2d48e
Omar Mirza <omirza@gmail.com>
2023-12-02 15:34:19 -0500

    Clicking logo now clears active project

--- app/src/components/FloatLogo.jsx
+++ app/src/components/FloatLogo.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { /* reducers */ } from "../deckhandSlice";
-import logo from '../assets/logo.png';
+import { setProjectId } from "../deckhandSlice";
+import logo from "../assets/logo.png";
 
 export default function FloatLogo() {
   const state = useSelector((state) => state.deckhand);
   const dispatch = useDispatch();
 
-  return <div className="logo">
-    <img src={logo} alt="Deckhand" />
-  </div>
+  return (
+    <div className="logo" onClick={() => dispatch(setProjectId(null))}>
+      <img src={logo} alt="Deckhand" />
+    </div>
+  );
 }
