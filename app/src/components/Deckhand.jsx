bc267e259f666f61756f305a7f6418027f72c847
Omar Mirza <omirza@gmail.com>
2023-12-02 15:36:39 -0500

    Built out deckhand page

--- app/src/components/Deckhand.jsx
+++ app/src/components/Deckhand.jsx
@@ -1,27 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { /* reducers */ } from "../deckhandSlice";
+// import { /* reducers */ } "../deckhandSlice";
 import FloatLogo from "./FloatLogo";
-import FloatTitle from "./FloatTitle";
+import FloatActiveProject from "./FloatActiveProject";
 import FloatAccount from "./FloatAccount";
 
 export default function Deckhand() {
   const state = useSelector((state) => state.deckhand);
   const dispatch = useDispatch();
+  const project = state.projects.find((obj) => obj.id === state.projectId);
 
-  return <div className="container">
-    <FloatLogo />
-    <FloatTitle />
-    <FloatAccount />
-    <div className="content-container">
-      <div className="content">
-        <h1>Cluster 1</h1>
-        <div id="project-cards">
-          <div className="new-card">
-            <div>New<br />Pod</div>
+  const clusterBundle = [];
+  if (project.clusters) {
+    for (let i = 0; i < project.clusters.length; i++) {
+      // bundle pods
+      const podBundle = [];
+      if (project.clusters[i]) {
+        for (const pod of project.clusters[i]) {
+          console.log(pod);
+          podBundle.push(
+            <div className="card">
+              <div className="name">{pod.type}</div>
+              <button>Add ConfigMap</button>
+              <button>Add Secrets</button>
+              <button>Add Ingress</button>
+              <button>Add Volume</button>
+              <button className="red">Delete Pod</button>
+            </div>
+          );
+        }
+      }
+
+      // bundle cluster
+      clusterBundle.push(
+        <>
+          <h1>Cluster {i + 1}</h1>
+          <div id="pod-cards">
+            <div className="new-card">
+              <div>New Pod</div>
+              <button>Add Github</button>
+              <button>Add Docker-Hub</button>
+            </div>
+            {podBundle}
           </div>
-        </div>
+        </>
+      );
+    }
+  }
+
+  return (
+    <div className="container">
+      <FloatLogo />
+      <FloatActiveProject />
+      <FloatAccount />
+      <div className="content-container">
+        <div className="content">{clusterBundle}</div>
       </div>
     </div>
-  </div>;
+  );
 }
