7cc0fcf5ad37e8edc254378b9a02249f7a5edde8
Omar Mirza <omirza@gmail.com>
2023-12-02 15:32:26 -0500

    Created FloatActiveProject, updated reference

---
+++ app/src/components/FloatActiveProject.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// import { /* reducers */ } "../deckhandSlice";
+
+export default function FloatActiveProject() {
+  const state = useSelector((state) => state.deckhand);
+  const dispatch = useDispatch();
+  const project = state.projects.find((obj) => obj.id === state.projectId);
+
+  return <div className="page-title">{project.name}</div>;
+}
