5827f13af4eaa4db5158c5ea660f895258a5496d
Omar Mirza <omirza@gmail.com>
2023-12-02 15:34:50 -0500

    Updated projects references

--- app/src/components/Projects.jsx
+++ app/src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setProject } from "../deckhandSlice";
+import { setProjectId } from "../deckhandSlice";
 import FloatLogo from "./FloatLogo";
 import FloatAccount from "./FloatAccount";
 
@@ -10,35 +10,42 @@ export default function Projects() {
 
   const handleClick = (id) => (e) => {
     e.preventDefault();
-    dispatch(setProject((id)));
+    dispatch(setProjectId(id));
   };
 
   const projectBundle = [];
-  for (const el of state.projectArr) {
+  for (const el of state.projects) {
     projectBundle.push(
       <div key={el.id} className="card" onClick={handleClick(el.id)}>
         <div className="name">{el.name}</div>
         <div className="date-info">
-          Created: {el.created_date}<br />
+          Created: {el.created_date}
+          <br />
           Last Modified: {el.modified_date}
         </div>
       </div>
-    )
+    );
   }
 
-  return <div className="container">
-    <FloatLogo />
-    <FloatAccount />
-    <div className="content-container">
-      <div className="content">
-        <h1>Home</h1>
-        <div id="project-cards">
-          <div className="new-card">
-            <div>New<br />Project</div>
+  return (
+    <div className="container">
+      <FloatLogo />
+      <FloatAccount />
+      <div className="content-container">
+        <div className="content">
+          <h1>Home</h1>
+          <div id="project-cards">
+            <div className="new-card">
+              <div>
+                New
+                <br />
+                Project
+              </div>
+            </div>
+            {projectBundle}
           </div>
-          {projectBundle}
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
