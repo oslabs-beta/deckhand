055dd56c2b14599e85caf5003d12954266f30f90
Omar Mirza <omirza@gmail.com>
2023-12-02 15:35:42 -0500

    Cluster state variable architecture

--- app/src/deckhandSlice.js
+++ app/src/deckhandSlice.js
@@ -4,28 +4,48 @@ export const deckhandSlice = createSlice({
   name: 'deckhand',
   initialState: {
     user: null,
-    // user: { name: 'John' }, // {id, name, photo_url, ...},
+    // user: { id: 1, name: 'John', avatar_url: 'http://example.com' }, // {id, name, avatar_url},
 
-    projectArr: [
-      { id: 1, name: 'Example App 1', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-      { id: 2, name: 'Example App 2', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-      { id: 3, name: 'Example App 3', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-      { id: 4, name: 'Example App 4', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-      { id: 5, name: 'Example App 5', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-      { id: 6, name: 'Example App 6', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023' },
-    ], // [ {id, name, created_date, modified_date, ...}, ... ]
+    projects: [
+      {
+        id: 1, name: 'Example App 1', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023',
+        clusters: [[
+          {
+            id: 1,
+            type: 'github',
+            configMap: null,
+            secrets: null,
+            ingress: null,
+            volume: null,
+          },
+          {
+            id: 2,
+            type: 'docker-hub',
+            configMap: null,
+            secrets: null,
+            ingress: null,
+            volume: null,
+          },
+        ]]
+      },
+      { id: 2, name: 'Example App 2', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
+      { id: 3, name: 'Example App 3', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
+      { id: 4, name: 'Example App 4', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
+      { id: 5, name: 'Example App 5', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
+      { id: 6, name: 'Example App 6', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
+    ],
 
-    project: null, // id
+    projectId: null, // id
   },
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload;
     },
-    setProjectArr: (state, action) => {
-      state.projectArr = action.payload;
+    setProjects: (state, action) => {
+      state.projects = action.payload;
     },
-    setProject: (state, action) => {
-      state.project = action.payload;
+    setProjectId: (state, action) => {
+      state.projectId = action.payload;
     },
   },
 });
@@ -33,8 +53,8 @@ export const deckhandSlice = createSlice({
 // Export actions for use in components
 export const {
   setUser,
-  setProjectArr,
-  setProject,
+  setProjects,
+  setProjectId,
 } = deckhandSlice.actions;
 
 // Export the reducer function for store configuration
