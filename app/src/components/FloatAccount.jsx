e3607aedc202377c63da35dc48de8d6c9e91fad2
Omar Mirza <omirza@gmail.com>
2023-12-03 22:47:18 -0500

    Added dropdown indicator to floating account

--- app/src/components/FloatAccount.jsx
+++ app/src/components/FloatAccount.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import { /* reducers */ } "../deckhandSlice";
 import account from "../assets/account.png";
+import Icon from "@mdi/react";
+import { mdiChevronDown } from "@mdi/js";
 
 export default function Deckhand() {
   const state = useSelector((state) => state.deckhand);
@@ -10,7 +12,8 @@ export default function Deckhand() {
   return (
     <div className="account">
       <span>{state.user.name}</span>
-      <img src={account} alt="Account" />
+      <img src={account} alt="Account" />{" "}
+      <Icon path={mdiChevronDown} size={1} />
     </div>
   );
 }
