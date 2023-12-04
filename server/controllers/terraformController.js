0f697c10857c4ce8dd48c1556393a4b04f324cbf
Omar Mirza <omirza@gmail.com>
2023-12-04 12:40:55 -0500

    Initial terraform api controller

---
+++ server/controllers/terraformController.js
@@ -0,0 +1,23 @@
+const terraformController = {};
+
+terraformController.addProject = (req, res, next) => {
+  next();
+};
+
+terraformController.deleteProject = (req, res, next) => {
+  next();
+};
+
+terraformController.addCluster = (req, res, next) => {
+  next();
+};
+
+terraformController.deleteCluster = (req, res, next) => {
+  next();
+};
+
+terraformController.updateCluster = (req, res, next) => {
+  next();
+};
+
+module.exports = terraformController;
