2a0009834c4642ff2aafccf4b6606dad53d1a902
Omar Mirza <omirza@gmail.com>
2023-12-03 21:41:10 -0500

    Refactored bundle logic

--- app/src/components/Project.jsx
+++ app/src/components/Project.jsx
@@ -11,71 +11,67 @@ export default function Project() {
   const project = state.projects.find((p) => p.id === state.projectId);
 
   const clusterBundle = [];
-  if (project.clusters) {
-    for (let i = 0; i < project.clusters.length; i++) {
-      // bundle pods
-      const podBundle = [];
-      if (project.clusters[i]) {
-        for (const pod of project.clusters[i].pods) {
-          podBundle.push(
-            <div className="card">
-              <div className="name">{pod.name}</div>
-              {!pod.config ? (
-                <>
-                  <button>Configure</button>
-                </>
+  for (const cluster of project.clusters) {
+    // bundle pods
+    const podBundle = [];
+    for (const pod of cluster.pods) {
+      podBundle.push(
+        <div className="card">
+          <div className="name">{pod.name}</div>
+          {!pod.config ? (
+            <>
+              <button>Configure</button>
+            </>
+          ) : (
+            <>
+              <button>
+                <b>Edit Config</b>
+              </button>
+              <button>
+                <b>Edit Replicas ({pod.replicas})</b>
+              </button>
+              {!pod.variables ? (
+                <button>Add Variables</button>
               ) : (
-                <>
-                  <button>
-                    <b>Edit Config</b>
-                  </button>
-                  <button>
-                    <b>Edit Replicas ({pod.replicas})</b>
-                  </button>
-                  {!pod.variables ? (
-                    <button>Add Variables</button>
-                  ) : (
-                    <button>
-                      <b>Edit Variables</b>
-                    </button>
-                  )}
-                  {!pod.ingress ? (
-                    <button>Add Ingress</button>
-                  ) : (
-                    <button>
-                      <b>Edit Ingress</b>
-                    </button>
-                  )}
-                  {!pod.volume ? (
-                    <button>Add Volume</button>
-                  ) : (
-                    <button>
-                      <b>Edit Volume</b>
-                    </button>
-                  )}
-                </>
+                <button>
+                  <b>Edit Variables</b>
+                </button>
               )}
-              <button className="delete">Delete Pod</button>
-            </div>
-          );
-        }
-      }
-
-      // bundle cluster
-      clusterBundle.push(
-        <>
-          <h2>{project.clusters[i].name}</h2>
-          <div id="pod-cards">
-            <div className="card">
-              <div className="new-pod">New Pod</div>
-              <button>+ Add Github</button>
-              <button>+ Add Docker-Hub</button>
-            </div>
-            {podBundle}
-          </div>
-        </>
+              {!pod.ingress ? (
+                <button>Add Ingress</button>
+              ) : (
+                <button>
+                  <b>Edit Ingress</b>
+                </button>
+              )}
+              {!pod.volume ? (
+                <button>Add Volume</button>
+              ) : (
+                <button>
+                  <b>Edit Volume</b>
+                </button>
+              )}
+            </>
+          )}
+          <button className="delete">Delete Pod</button>
+        </div>
       );
     }
+
+    // bundle clusters
+    clusterBundle.push(
+      <>
+        <h2>{cluster.name}</h2>
+        <div id="pod-cards">
+          <div className="card">
+            <div className="new-pod">New Pod</div>
+            <button>+ Add Github</button>
+            <button>+ Add Docker-Hub</button>
+          </div>
+          {podBundle}
+        </div>
+      </>
+    );
   }
 
   return (
