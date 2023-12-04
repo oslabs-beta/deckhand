5968f211c0b6c6a72279cd0f6523cd5ae12f3219
Omar Mirza <omirza@gmail.com>
2023-12-03 20:56:16 -0500

    Add/delete project functionality

--- app/src/components/Projects.jsx
+++ app/src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setProjectId } from "../deckhandSlice";
+import { setProjectId, addProject, deleteProject } from "../deckhandSlice";
 import FloatLogo from "./FloatLogo";
 import FloatAccount from "./FloatAccount";
 
@@ -8,21 +8,39 @@ export default function Projects() {
   const state = useSelector((state) => state.deckhand);
   const dispatch = useDispatch();
 
-  const handleClick = (id) => (e) => {
-    e.preventDefault();
+  const handleClickAddProject = (event) => {
+    const id = state.projects.length + 1;
+    const createdDate = "Dec 4, 2023";
+    const modifiedDate = "Dec 4, 2023";
+    dispatch(addProject({ id, createdDate, modifiedDate }));
     dispatch(setProjectId(id));
   };
 
   const projectBundle = [];
-  for (const el of state.projects) {
+  for (const el of [...state.projects].reverse()) {
     projectBundle.push(
-      <div key={el.id} className="card" onClick={handleClick(el.id)}>
+      <div
+        key={el.id}
+        className="card"
+        onClick={() => dispatch(setProjectId(el.id))}
+      >
         <div className="name">{el.name}</div>
         <div className="date-info">
           Created: {el.createdDate}
           <br />
           Last Modified: {el.modifiedDate}
+          <br />
+          <br />
         </div>
+        <button
+          className="delete"
+          onClick={(e) => {
+            dispatch(deleteProject(el.id));
+            e.stopPropagation();
+          }}
+        >
+          Delete Project
+        </button>
       </div>
     );
   }
@@ -35,7 +53,7 @@ export default function Projects() {
         <div className="content">
           <h1>Home</h1>
           <div id="project-cards">
-            <div className="card">
+            <div className="card" onClick={handleClickAddProject}>
               <div className="new-project">
                 New
                 <br />
