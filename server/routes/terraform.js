60a156a460b87ebcb7a66ddf5389696f9cee8c67
Omar Mirza <omirza@gmail.com>
2023-12-04 12:41:10 -0500

    Initial terraform api routes

---
+++ server/routes/terraform.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const terraformController = require('../controllers/terraformController');
+
+router.post('/addProject', terraformController.addProject, (req, res) => {
+  res.status(200)//.json(res.locals.data);
+});
+
+router.post('/deleteProject', terraformController.deleteProject, (req, res) => {
+  res.status(200)//.json(res.locals.data);
+});
+
+router.post('/addCluster', terraformController.addCluster, (req, res) => {
+  res.status(200)//.json(res.locals.data);
+});
+
+router.post('/deleteCluster', terraformController.deleteCluster, (req, res) => {
+  res.status(200)//.json(res.locals.data);
+});
+
+router.post('/updateCluster', terraformController.updateCluster, (req, res) => {
+  res.status(200)//.json(res.locals.data);
+});
+
+module.exports = router;
