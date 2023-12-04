44b975484d28d040aba432ca9971962e09bc705a
Omar Mirza <omirza@gmail.com>
2023-12-04 16:24:59 -0500

    Updated terraform routes based on res.locals

--- server/routes/terraform.js
+++ server/routes/terraform.js
@@ -3,23 +3,23 @@ const router = express.Router();
 const terraformController = require('../controllers/terraformController');
 
 router.post('/addProject', terraformController.addProject, (req, res) => {
-  res.status(200)//.json(res.locals.data);
+  res.status(200).json(res.locals.data);
 });
 
 router.post('/deleteProject', terraformController.deleteProject, (req, res) => {
-  res.status(200)//.json(res.locals.data);
+  res.sendStatus(200);
 });
 
 router.post('/addCluster', terraformController.addCluster, (req, res) => {
-  res.status(200)//.json(res.locals.data);
+  res.status(200).json(res.locals.data);
 });
 
 router.post('/deleteCluster', terraformController.deleteCluster, (req, res) => {
-  res.status(200)//.json(res.locals.data);
+  res.sendStatus(200);
 });
 
 router.post('/configureCluster', terraformController.configureCluster, (req, res) => {
-  res.status(200)//.json(res.locals.data);
+  res.sendStatus(200);
 });
 
 module.exports = router;
