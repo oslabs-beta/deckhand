d9db60a76ebc9040b217beeb4ed05a196cb26ebd
Omar Mirza <omirza@gmail.com>
2023-12-03 20:55:08 -0500

    Cluster config state, add/del projects, clusters

--- app/src/deckhandSlice.js
+++ app/src/deckhandSlice.js
@@ -4,48 +4,88 @@ export const deckhandSlice = createSlice({
   name: 'deckhand',
   initialState: {
     user: null,
-    // user: { id: 1, name: 'John', avatar_url: 'http://example.com' }, // {id, name, avatar_url},
+    // user: { id: 1, name: 'John', avatarUrl: 'http://example.com' },
 
     projects: [
       {
-        id: 1, name: 'Example App 1', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023',
-        clusters: [[
+        id: 1,
+        name: 'Project 1',
+        createdDate: 'Nov 29, 2023',
+        modifiedDate: 'Nov 30, 2023',
+        clusters: [
           {
             id: 1,
-            type: 'github',
-            configMap: null,
-            secrets: null,
-            ingress: null,
-            volume: null,
+            name: 'Cluster 1',
+            config: null, // {provider: 'AWS', name: '', region: 'US-East'},
+            pods: [
+              {
+                id: 1,
+                name: 'Pod 1 (github)',
+                type: 'github',
+                config: null, // {url: 'http://example.com', build: '1.0.5', branch: 'main'}
+                replicas: 1,
+                variables: null, // [{key: value, secret as Boolean}, ...]
+                ingress: null, // port number
+                volume: null, // directory string
+              },
+              {
+                id: 2,
+                name: 'Pod 2 (docker-hub)',
+                type: 'docker-hub',
+                config: null, // {url: 'http://example.com', version: '3.5.1'}
+                replicas: 1,
+                variables: null, // [{key: value, secret as Boolean}, ...]
+                ingress: null, // port number
+                volume: null, // directory string
+              },
+            ]
           },
-          {
-            id: 2,
-            type: 'docker-hub',
-            configMap: null,
-            secrets: null,
-            ingress: null,
-            volume: null,
-          },
-        ]]
+        ]
       },
-      { id: 2, name: 'Example App 2', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
-      { id: 3, name: 'Example App 3', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
-      { id: 4, name: 'Example App 4', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
-      { id: 5, name: 'Example App 5', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
-      { id: 6, name: 'Example App 6', created_date: 'Nov 29, 2023', modified_date: 'Nov 30, 2023', clusters: [[]] },
     ],
 
     projectId: null, // id
   },
   reducers: {
-    setUser: (state, action) => {
+    setUser: (state, action) => { // payload: {id, name, avatarUrl}
       state.user = action.payload;
     },
-    setProjects: (state, action) => {
+    setProjects: (state, action) => { // payload: (full state object from SQL)
       state.projects = action.payload;
     },
-    setProjectId: (state, action) => {
+    setProjectId: (state, action) => { // payload: id
       state.projectId = action.payload;
+    },
+    addProject: (state, action) => { // payload: {id, createdDate, modifiedDate}
+      const { id, createdDate, modifiedDate } = action.payload;
+      state.projects.push({
+        id: id, // change to projectId from SQL once connected
+        name: `Project ${state.projects.length + 1}`,
+        createdDate: createdDate,
+        modifiedDate: modifiedDate,
+        clusters: [
+          {
+            id: 1,
+            name: 'Cluster 1',
+            config: null,
+            pods: [],
+          }
+        ],
+      });
+    },
+    deleteProject: (state, action) => { // payload: id
+      state.projects = state.projects.filter(project => project.id !== action.payload)
+    },
+    addCluster: (state, action) => { // payload: {projectId, clusterId}
+      const { projectId, clusterId } = action.payload;
+      const project = state.projects.find(p => p.id === projectId);
+      // project.modifiedDate = (today's date) or get from SQL?
+      project.clusters.push({
+        id: clusterId, // change to clusterId from SQL once connected
+        name: `Cluster ${project.clusters.length + 1}`,
+        config: null,
+        pods: [],
+      });
     },
   },
 });
@@ -55,6 +95,9 @@ export const {
   setUser,
   setProjects,
   setProjectId,
+  addProject,
+  deleteProject,
+  addCluster,
 } = deckhandSlice.actions;
 
 // Export the reducer function for store configuration
