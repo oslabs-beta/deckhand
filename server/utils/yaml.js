cd0686bf6ed9b1c210239d7e2f47bce34dacdbb6
Omar Mirza <omirza@gmail.com>
2023-12-04 13:12:14 -0500

    Created yaml generator function for ConfigMaps

--- server/utils/yaml.js
+++ server/utils/yaml.js
@@ -35,4 +35,23 @@ createYaml.ingress = (name, host, path, serviceName, servicePort) => {
 // const ingressYAML = createYaml.ingress('my-ingress', 'example.com', '/path', 'my-service', 80);
 // console.log(ingressYAML);
 
+createYaml.configMap = (name, data) => {
+  return YAML.stringify({
+    apiVersion: 'v1',
+    kind: 'ConfigMap',
+    metadata: {
+      name: name
+    },
+    data: data
+  });
+};
+
+// Example usage
+// const data = {
+//   'key1': 'value1',
+//   'key2': 'value2'
+// };
+// const configMapYAML = createYaml.configMap('my-configmap', data);
+// console.log(configMapYAML);
+
 module.exports = createYaml;
