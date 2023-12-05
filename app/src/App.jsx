570b37a0e272f88335d2504cd53dc92a3109d0d2
Omar Mirza <omirza@gmail.com>
2023-12-04 21:55:28 -0500

    Cleaned up unused imports

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import { setUser } from "./deckhandSlice";
+import /* reducers */ "./deckhandSlice";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Project from "./components/Project";
