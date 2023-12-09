d7f2350d0fdff0e790d3fde447882f18868c6d0f
Dennis Corsi <denniscorsi@gmail.com>
2023-12-09 15:48:55 -0500

    Completed env scanning logic

--- TempScan.js
+++ TempScan.js
@@ -15,39 +15,47 @@ execSync(`cd ${tempsPath} && git clone ${repoUrl}`);
 const repoPath = path.join(tempsPath, repoName);
 
 // will fill an array of all file names nested within the repo
-const fileNames = [];
+const filePaths = [];
 const getFiles = (dir) => {
-  const contents = fs.readdirSync(repoPath); // This will return an array with names of all files and directories, not differentiated
-  contents.forEach((content) => { 
-    const contentPath = path.join(dir, content);
-    const stats = fs.statSync(contentPath);
-    if (stats.isDirectory()) {
-      getFiles(contentPath);
-    } else fileNames.push(contentPath);
+  console.log('----');
+  console.log('DIR:', dir);
+  const contents = fs.readdirSync(dir); // This will return an array with names of all files and directories, not differentiated
+  console.log('CONTENTS:', contents);
+  contents.forEach((content) => {
+    if (content !== '.git') {
+      // find better name for content
+      const contentPath = path.join(dir, content);
+      const stats = fs.statSync(contentPath);
+      if (stats.isDirectory()) {
+        getFiles(contentPath);
+      } else filePaths.push(contentPath);
+    }
   });
 };
 
 getFiles(repoPath);
 
-console.log(fileNames);
+console.log('FILEPATHS:', filePaths);
 
 const files = [];
 const envs = [];
 
-// fileNames.forEach((fileName) => {
-//   files.push(fs.readFileSync(`${path}/${filename}`, 'utf8'));
-// });
+filePaths.forEach((filePath) => {
+  files.push(fs.readFileSync(filePath, 'utf8'));
+});
 
-// files.forEach((file) => {
-//   const regex = /([A-Za-z0-9$_]+)[\s=]+process.env./g; // g tag means find all matches
-//   let result;
-//   while ((result = regex.exec(contents))) {
-//     //result will be null if there are no more to be found
-//     envs.push(result[1]);
-//   }
-// });
+console.log('FILES:', files);
 
-// console.log(envs);
+files.forEach((file) => {
+  const regex = /([A-Za-z0-9$_]+)[\s=]+process.env./g; // g tag means find all matches
+  let result;
+  while ((result = regex.exec(file))) {
+    //result will be null if there are no more to be found
+    envs.push(result[1]);
+  }
+});
+
+console.log('ENVS:', envs); // should be [key, $val_2, validation, secret_messsage, uri]
 
 // [any character that is NOT a letter, number, underscore, or dollar sign]
 // [then a string of characters that ARE a letter, number, underscore, or dollar sign]
