57182dd31a18f4137aa8e15f0071797fbf90ce51
Dennis Corsi <denniscorsi@gmail.com>
2023-12-09 16:02:36 -0500

    Cleaned and commented

--- TempScan.js
+++ TempScan.js
@@ -2,66 +2,59 @@ const fs = require('fs');
 const path = require('path');
 const { execSync, exec } = require('child_process');
 
-//need nested files too
-
 const repoUrl = 'https://github.com/denniscorsi/envs.git';
 const repoName = 'envs';
 
 // Clones repo into the temps folder
 const tempsPath = path.join(__dirname, 'server', 'temps');
-console.log(tempsPath);
 execSync(`cd ${tempsPath} && git clone ${repoUrl}`);
-
 const repoPath = path.join(tempsPath, repoName);
 
-// will fill an array of all file names nested within the repo
+// An array to hold the paths of all files nested within the repo
 const filePaths = [];
+
+// Recursive helper function to get all nested files
 const getFiles = (dir) => {
-  console.log('----');
-  console.log('DIR:', dir);
-  const contents = fs.readdirSync(dir); // This will return an array with names of all files and directories, not differentiated
-  console.log('CONTENTS:', contents);
-  contents.forEach((content) => {
+  const dirContents = fs.readdirSync(dir); // This will return an array with names of all files and directories in the top level of the directory
+
+  // Ignoring anything within the .git directory, check if each content if a file or directory
+  // If it's a file, push its path to filePaths
+  // If it's a directory, send it recursively back into this function
+  dirContents.forEach((content) => {
     if (content !== '.git') {
-      // find better name for content
       const contentPath = path.join(dir, content);
       const stats = fs.statSync(contentPath);
-      if (stats.isDirectory()) {
-        getFiles(contentPath);
-      } else filePaths.push(contentPath);
+      if (stats.isDirectory()) getFiles(contentPath);
+      else filePaths.push(contentPath);
     }
   });
 };
 
+// Execute function on the cloned repo
 getFiles(repoPath);
 
-console.log('FILEPATHS:', filePaths);
-
-const files = [];
+const fileContents = [];
 const envs = [];
 
+// Push the text content of each file into the fileContents array
 filePaths.forEach((filePath) => {
-  files.push(fs.readFileSync(filePath, 'utf8'));
+  fileContents.push(fs.readFileSync(filePath, 'utf8'));
 });
 
-console.log('FILES:', files);
-
-files.forEach((file) => {
-  const regex = /([A-Za-z0-9$_]+)[\s=]+process.env./g; // g tag means find all matches
-  let result;
-  while ((result = regex.exec(file))) {
-    //result will be null if there are no more to be found
+// Using regex, scan the text of each file for environmental variables and push them to envs array.
+fileContents.forEach((file) => {
+  const regex = /([A-Za-z0-9$_]+)[\s=]+process.env./g;
+  let result = regex.exec(file);
+  while (result) {
     envs.push(result[1]);
+    result = regex.exec(file);
   }
 });
 
 console.log('ENVS:', envs); // should be [key, $val_2, validation, secret_messsage, uri]
 
+// Regex parameters:
 // [any character that is NOT a letter, number, underscore, or dollar sign]
 // [then a string of characters that ARE a letter, number, underscore, or dollar sign]
 // [then either a space or an equal sign (any number of these)]
 // [then the full string 'process.env.']
-
-// we need the variable *in front* of the process.env
-
-// indices.push(result.index);
