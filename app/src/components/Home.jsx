e2d6c459c88622f93db97896c5f3855c19c75342
Omar Mirza <omirza@gmail.com>
2023-12-14 19:49:22 -0500

    Add dropdown to Home

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setProjectId, addProject, deleteProject } from "../deckhandSlice";
+import {
+  setProjectId,
+  addProject,
+  deleteProject,
+  showModal,
+} from "../deckhandSlice";
 import FloatLogo from "./floats/FloatLogo";
 import FloatAccount from "./floats/FloatAccount";
+import Modals from "./modals/Modals";
+import Icon from "@mdi/react";
+import { mdiDotsVertical } from "@mdi/js";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 export default function Home() {
   const state = useSelector((state) => state.deckhand);
@@ -26,6 +35,17 @@ export default function Home() {
     else return Math.round(days) + " days ago";
   };
 
+  const handleAction = async (key, project) => {
+    console.log(key);
+    console.log(project);
+    if (key === "configure-project") {
+      dispatch(showModal({ name: "ConfigureProject", data: project }));
+    }
+    if (key === "delete-project") {
+      dispatch(deleteProject(project.projectId));
+    }
+  };
+
   const projectBundle = [];
   const sortedProjects = [...state.projects].sort((a, b) => {
     const dateA = new Date(a.modifiedDate);
@@ -39,6 +59,35 @@ export default function Home() {
         className="card"
         onClick={() => dispatch(setProjectId(el.projectId))}
       >
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button className="card-menu" aria-label="Customise options">
+              <Icon path={mdiDotsVertical} size={1} />
+            </button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className="dropdown"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <DropdownMenu.Item
+                className="dropdown-item"
+                onClick={() => {
+                  dispatch(showModal({ name: "ConfigureProject", data: el }));
+                }}
+              >
+                Configure
+              </DropdownMenu.Item>
+              <DropdownMenu.Separator className="dropdown-separator" />
+              <DropdownMenu.Item
+                className="dropdown-item"
+                onClick={() => dispatch(deleteProject(el.projectId))}
+              >
+                Delete
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
         <div className="name">{el.name}</div>
         <div
           className="date-info"
@@ -52,16 +101,6 @@ export default function Home() {
         >
           Last Modified: {timeAgo(el.modifiedDate)}
         </div>
-        <br />
-        <button
-          className="delete"
-          onClick={(e) => {
-            dispatch(deleteProject(el.projectId));
-            e.stopPropagation();
-          }}
-        >
-          Delete Project
-        </button>
       </div>
     );
   }
@@ -70,6 +109,7 @@ export default function Home() {
     <div className="container">
       <FloatLogo />
       <FloatAccount />
+      <Modals />
       <div className="content-container">
         <div className="content">
           <h1>Home</h1>
