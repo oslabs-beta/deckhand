0b66b16ca6469f00e5d5f1c507e34facae87a8e7
Omar Mirza <omirza@gmail.com>
2023-12-16 10:39:05 -0500

    Use svg logo in float

--- app/src/components/floats/FloatLogo.jsx
+++ app/src/components/floats/FloatLogo.jsx
@@ -8,8 +8,70 @@ export default function FloatLogo() {
   const dispatch = useDispatch();
 
   return (
-    <div className="logo" onClick={() => dispatch(setProjectId(null))}>
-      <img src={logo} alt="Deckhand" />
+    <div className="float-logo" onClick={() => dispatch(setProjectId(null))}>
+      <svg
+        version="1.1"
+        class="logo"
+        baseProfile="tiny"
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        viewBox="0 0 500 500"
+        style={{ width: "40px", height: "40px" }}
+      >
+        <circle
+          cx="250"
+          cy="250"
+          r="200"
+          fill="none"
+          stroke="#333"
+          stroke-width="40"
+        />
+
+        <circle
+          cx="250"
+          cy="250"
+          r="60"
+          fill="#333"
+          stroke="none"
+          stroke-width="40"
+        />
+
+        <line
+          x1="250"
+          y1="0"
+          x2="250"
+          y2="500"
+          stroke="#333"
+          stroke-width="40"
+        />
+        <line
+          x1="0"
+          y1="250"
+          x2="500"
+          y2="250"
+          stroke="#333"
+          stroke-width="20"
+        />
+        <line
+          x1="35.36"
+          y1="35.36"
+          x2="464.64"
+          y2="464.64"
+          stroke="#333"
+          stroke-width="20"
+        />
+        <line
+          x1="464.64"
+          y1="35.36"
+          x2="35.36"
+          y2="464.64"
+          stroke="#333"
+          stroke-width="20"
+        />
+      </svg>
+      <span>Deckhand</span>
     </div>
   );
 }
