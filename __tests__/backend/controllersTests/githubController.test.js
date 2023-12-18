8f8fe9c31022fdd19371f62d0ada8c90093d0f71
Dennis Corsi <denniscorsi@gmail.com>
2023-12-18 11:53:09 -0500

    Start test file

--- __tests__/backend/controllersTests/githubController.test.js
+++ __tests__/backend/controllersTests/githubController.test.js
@@ -1,11 +1,24 @@
 const request = require('supertest');
 const githubController = require('../../../server/controllers/githubController.js');
 
-describe('testing Github callback component', () => {
+// describe('testing Github callback component', () => {
 
-  test('check if query code token is in the request', () => {
-    const req = {query: {code: 'ur5j49dk2or5z34hg4p9'}};
-    expect(req.query.code).toHaveLength(20);
+//   test('check if query code token is in the request', () => {
+//     const req = {query: {code: 'ur5j49dk2or5z34hg4p9'}};
+//     expect(req.query.code).toHaveLength(20);
+//   });
+
+// });
+
+describe('scan tests', () => {
+  it('Should return undefined if there is no dockerfile', () => {
+    console.log('hello!');
+    expect(1).toEqual(1);
   });
+
+  it('Should return undefined if there is no exposed port in the dockerfile', () => {})
+
+  it('Should return X', () => {})
+
 
 });
