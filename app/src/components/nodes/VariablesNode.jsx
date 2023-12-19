14b15d4614c3d24d805912c783af4393fbf89e23
Omar Mirza <omirza@gmail.com>
2023-12-19 12:22:32 -0500

    Add dropdown to variables node

--- app/src/components/nodes/VariablesNode.jsx
+++ app/src/components/nodes/VariablesNode.jsx
@@ -1,22 +1,12 @@
 import React, { useEffect, useState, useCallback, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Handle, Position } from "reactflow";
-import {
-  showModal,
-  addCluster,
-  deleteCluster,
-  addPod,
-  deletePod,
-  configurePod,
-  addVarSet,
-  addIngress,
-  addVolume,
-} from "../../deckhandSlice";
+import { showModal, deleteNode } from "../../deckhandSlice";
 import Icon from "@mdi/react";
 import { mdiDotsVertical, mdiKeyVariant } from "@mdi/js";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
-export default function ({ data, isConnectable }) {
+export default function ({ id, data, isConnectable }) {
   const state = useSelector((state) => state.deckhand);
   const dispatch = useDispatch();
 
@@ -28,10 +18,47 @@ export default function ({ data, isConnectable }) {
         isConnectable={isConnectable}
       />
       <div>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button className="node-menu" aria-label="Customise options">
+              <Icon path={mdiDotsVertical} size={1} />
+            </button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className="dropdown"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <DropdownMenu.Item
+                className="dropdown-item"
+                onClick={() =>
+                  dispatch(showModal({ name: "ConfigureVarSets", id, data }))
+                }
+              >
+                Edit Variables
+              </DropdownMenu.Item>
+              <DropdownMenu.Separator className="dropdown-separator" />
+              <DropdownMenu.Item
+                className="dropdown-item"
+                onClick={() => dispatch(deleteNode(id))}
+              >
+                Delete
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
         <div className="icon">
           <Icon path={mdiKeyVariant} style={{ color: "orange" }} size={1} />
         </div>
-        <div className="title">{"Variables"}</div>
+        <div className="title">Variables</div>
+        <button
+          className="button nodrag"
+          onClick={() =>
+            dispatch(showModal({ name: "ConfigureVarSets", id, data }))
+          }
+        >
+          Edit Variables
+        </button>
       </div>
       <Handle
         type="source"
