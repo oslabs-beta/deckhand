e6a10e39916fa825c7e7dba261cf2383e9ce774e
Omar Mirza <omirza@gmail.com>
2023-12-18 21:51:32 -0500

    Build github node component

---
+++ app/src/components/nodes/GithubNode.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState, useCallback, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Handle, Position } from "reactflow";
+import {
+  showModal,
+  addCluster,
+  deleteCluster,
+  addPod,
+  deletePod,
+  configurePod,
+  addVarSet,
+  addIngress,
+  addVolume,
+} from "../../deckhandSlice";
+import Icon from "@mdi/react";
+import { mdiDotsVertical, mdiGithub } from "@mdi/js";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+
+export default function ({ data, isConnectable }) {
+  const state = useSelector((state) => state.deckhand);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="node">
+      <Handle
+        type="target"
+        position={Position.Top}
+        isConnectable={isConnectable}
+      />
+      <div>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button className="node-menu" aria-label="Customise options">
+              <Icon path={mdiDotsVertical} size={1} />
+            </button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className="dropdown"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <DropdownMenu.Item
+                className="dropdown-item"
+                onClick={() => dispatch(showModal({ name: "PodSource", data }))}
+              >
+                Select Source
+              </DropdownMenu.Item>
+              <DropdownMenu.Separator className="dropdown-separator" />
+              <DropdownMenu.Item
+                className="dropdown-item"
+                onClick={() => dispatch(deletePod(data.podId))}
+              >
+                Delete
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
+        <div className="icon">
+          <Icon path={mdiGithub} style={{ color: "black" }} size={1} />
+        </div>
+        <div className="title">{data.name}</div>
+        <button
+          className="button"
+          onClick={() => dispatch(showModal({ name: "PodSource", data }))}
+        >
+          Select Source
+        </button>
+      </div>
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="b"
+        isConnectable={isConnectable}
+      />
+    </div>
+  );
+}
