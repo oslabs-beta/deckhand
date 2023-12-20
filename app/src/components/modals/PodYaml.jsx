297d4557ce03cdd4ef473b49adecc7289dd36700
Omar Mirza <omirza@gmail.com>
2023-12-20 11:09:56 -0500

    Update pod yaml modal for node based architecture

--- app/src/components/modals/PodYaml.jsx
+++ app/src/components/modals/PodYaml.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { showModal, configurePod } from "../../deckhandSlice";
+import { showModal, updateNode } from "../../deckhandSlice";
 import "./modal.css";
 import createYaml from "../../yaml";
 
@@ -11,16 +11,8 @@ export default function () {
     setShow(false);
     setTimeout(() => dispatch(showModal({})), 300);
   };
-  const pod = state.modal.data;
-  const varSet = state.varSets.find(
-    (varSet) => varSet.varSetId === pod.varSetId
-  );
-  const ingress = state.ingresses.find(
-    (ingress) => ingress.ingressId === pod.ingressId
-  );
-  const volume = state.volumes.find(
-    (volume) => volume.volumeId === pod.volumeId
-  );
+  const id = state.modal.id;
+  const data = state.modal.data;
 
   const [show, setShow] = useState(false);
 
@@ -39,11 +31,11 @@ export default function () {
         <span className="close-button" onClick={closeModal}>
           &times;
         </span>
-        <h2>YAML File for {pod.name}</h2>
+        <h2>YAML File for {data.name}</h2>
         <form onSubmit={handleSubmit}>
           <label>
             <pre className="yaml" name="yaml">
-              {createYaml.all(pod)}
+              {createYaml.all(pods)}
             </pre>
           </label>
         </form>
