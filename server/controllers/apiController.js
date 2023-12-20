23e2d6bcca45658bc2b08e06fb0d18b1e38d7a92
odinolson17 <odinolsonsbooks@gmail.com>
2023-12-20 14:22:55 -0500

    Grabbed the port of the image from DockerHub

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,3 +1,4 @@
+const { execSync, exec } = require('child_process');
 const apiController = {};
 
 apiController.getProjects = (req, res, next) => {
@@ -31,6 +32,46 @@ apiController.getDockerHubImageTags = async (req, res, next) => {
       next();
     })
     .catch((error) => next(error));
+};
+
+// function to push docker images from Dockerhub
+
+apiController.pushDockerHubImagesToKluster = async (req, res, next) => {
+
+  // what is needed: the name of the github image to pull
+    // for example: deckhandapp/ideastation
+  // the tag at the end (without it assumes latest)
+    // for example: deckhandapp/ideastation:1 (important since this image has no latest)
+
+  const { repo_name } = req.body;
+  const { image } = req.body;
+  const { tag } = req.body;
+
+  //could only get the command to work with execSync as of right now
+
+  execSync(`docker image pull ${repo_name}/${image}:${tag}`);
+  const imageInformation = execSync(`docker image inspect ${repo_name}/${image}:${tag}`, {
+    encoding: 'utf8',
+  });
+  const imageInformationAsJSON = JSON.parse(imageInformation);
+  // this shows us the architecture of the image
+  console.log('show arch', imageInformationAsJSON[0].Architecture);
+  
+  const imageArchitecture = imageInformationAsJSON[0].Architecture;
+  // this gives us the port in an object 
+  console.log('show port', imageInformationAsJSON[0].Config.ExposedPorts);
+
+  const imagePortAsAnObject = imageInformationAsJSON[0].Config.ExposedPorts;
+  const imagePortAsAKey = Object.keys(imagePortAsAnObject);
+
+  console.log(imagePortAsAKey);
+
+  const imagePortAsString = imagePortAsAKey[0].match(/\d+/g);
+  const imagePort = Number(imagePortAsString);
+
+  console.log(imagePort);
+
+  return next();
 };
 
 module.exports = apiController;
