5b9683c449a5e13ea907f85e2f3d53e6936a6d8e
Omar Mirza <omirza@gmail.com>
2023-12-21 12:46:15 -0500

    Add 3 min wait to detDockerHubExposedPort tests

--- __tests__/backend/controllersTests/apiController.test.js
+++ __tests__/backend/controllersTests/apiController.test.js
@@ -14,26 +14,26 @@ describe('Testing the functionality of the "getDockerHubExposedPort" method', ()
     req.body.imageName = 'deckhandapp/ideastation';
     req.body.imageTag = '1';
     return expect(apiController.getDockerHubExposedPort(req, res, next)).resolves.toBe("Wrong type of image architecture");
-  });
+  }, 180000);
 
   it('See if image architecture is correct', async () => {
     req.body.imageName = 'mongo';
     req.body.imageTag = 'latest';
     return expect(apiController.getDockerHubExposedPort(req, res, next)).resolves.not.toBe("Wrong type of image architecture");
-  });
+  }, 180000);
 
   it('See if image port is found and correct', async () => {
     req.body.imageName = 'mongo';
     req.body.imageTag = 'latest';
     apiController.getDockerHubExposedPort(req, res, next)
     return expect(res.locals.data).toEqual({ "exposedPort": 27017 });
-  });
+  }, 180000);
 
   it('See if image port is found and expected is incorrect', async () => {
     req.body.imageName = 'mongo';
     req.body.imageTag = 'latest';
     apiController.getDockerHubExposedPort(req, res, next)
     return expect(res.locals.data).not.toEqual({ "exposedPort": 3000 });
-  });
+  }, 180000);
 
 });
