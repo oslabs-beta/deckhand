53cb5ad0a0e6856730ce82c13aef15bbe29042fb
Omar Mirza <omirza@gmail.com>
2023-12-21 10:09:18 -0500

    Add endpoint to update SQL database with current state

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,7 +1,32 @@
+const db = require('../data/model.js');
+
 const apiController = {};
 
-apiController.getProjects = (req, res, next) => {
-  next();
+apiController.updateDatabase = async (req, res, next) => {
+  const { id, name, email, avatarUrl, githubId, awsAccessKey, awsSecretKey, state } = req.body;
+
+  // Prepare the SQL query
+  const updateQuery = `
+    UPDATE users
+    SET name = $2, email = $3, avatarUrl = $4, githubId = $5, awsAccessKey = $6, awsSecretKey = $7, state = $8
+    WHERE _id = $1
+    RETURNING *
+  `;
+
+  try {
+    // Execute the query with the provided values
+    const result = await db.query(updateQuery, [id, name, email, avatarUrl, githubId, awsAccessKey, awsSecretKey, state]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    // Return the updated user data
+    // res.locals.data = result.rows[0];
+    return next();
+  } catch (error) {
+    next(error)
+  }
 };
 
 apiController.getDockerHubImages = async (req, res, next) => {
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const apiController = require('../controllers/apiController');
 
-router.post('/', apiController.getProjects, (req, res) => {
-  res.status(200)//.json(res.locals.data);
+router.post('/updateDatabase', apiController.updateDatabase, (req, res) => {
+  res.sendStatus(200)//.json(res.locals.data);
 });
 
 router.get('/dockerHubImages/*', apiController.getDockerHubImages, (req, res) => {
