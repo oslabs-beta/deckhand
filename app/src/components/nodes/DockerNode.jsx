55b514eef01d4920339b8d9042ec7be1155445d3
Dennis Corsi <denniscorsi@gmail.com>
2024-01-03 19:43:56 -0500

    fix awsClusterName

--- app/src/components/nodes/DockerNode.jsx
+++ app/src/components/nodes/DockerNode.jsx
@@ -122,7 +122,7 @@ export default function ({ id, data, isConnectable }) {
           awsAccessKey: state.user.awsAccessKey,
           awsSecretKey: state.user.awsSecretKey,
           vpcRegion: project.vpcRegion,
-          clusterName: cluster.data.awsClusterName,
+          awsClusterName: cluster.data.awsClusterName,
           yaml: yaml,
         }),
       });
@@ -145,7 +145,7 @@ export default function ({ id, data, isConnectable }) {
           awsAccessKey: state.user.awsAccessKey,
           awsSecretKey: state.user.awsSecretKey,
           vpcRegion: project.vpcRegion,
-          clusterName: cluster.awsClusterName,
+          awsClusterName: cluster.awsClusterName,
         }),
       });
 
