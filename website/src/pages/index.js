93f51ae41577bda88a8952e8436bf79416d0cd23
Omar Mirza <omirza@gmail.com>
2024-01-08 22:07:45 -0500

    Update preview images

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -3,7 +3,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import preview from '@site/static/img/preview.png';
+import preview from '@site/static/img/preview2.png';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
