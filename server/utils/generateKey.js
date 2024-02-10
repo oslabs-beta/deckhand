dd7918f1ada3f4ba99d391a5e2de153608f3ea94
Dennis Corsi <denniscorsi@gmail.com>
2024-02-09 19:18:32 -0500

    Ran ts-migrate rename

--- server/utils/generateKey.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a 32-byte random key for AES-256
-const key = crypto.randomBytes(32);
-console.log(key.toString('hex'));
-
-// In .env file, set ENCRYPTION_KEY=key (replace key with log value)
