af785f2945475a5914cca6079d79a5960b5f409b
Dennis Corsi <denniscorsi@gmail.com>
2024-02-09 20:10:13 -0500

    Add useState typing to PodSource

--- app/src/components/modals/PodSource.tsx
+++ app/src/components/modals/PodSource.tsx
@@ -1,166 +1,167 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { showModal, updateNode } from "../../deckhandSlice";
-import "./modal.css";
+import React, { ReactElement, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { showModal, updateNode } from '../../deckhandSlice';
+import './modal.css';
 
 export default function () {
   // @ts-expect-error TS(2571) FIXME: Object is of type 'unknown'.
   const state = useSelector((state) => state.deckhand);
   const dispatch = useDispatch();
   const closeModal = () => {
-    // @ts-expect-error TS(2345) FIXME: Argument of type '""' is not assignable to paramet... Remove this comment to see the full error message
-    setShow("");
+    setShow(false);
     setTimeout(() => dispatch(showModal({})), 300);
   };
   const id = state.modal.id;
   const data = state.modal.data;
 
-  const [show, setShow] = useState(false);
-  const [type, setType] = useState("docker-hub");
-  const [dockerHubImages, setDockerHubImages] = useState([]);
-  const [userRepos, setUserRepos] = useState([]);
-  const [publicRepos, setPublicRepos] = useState([]);
+  const [show, setShow] = useState<Boolean>(false);
+  const [type, setType] = useState<String>('docker-hub');
+  const [dockerHubImages, setDockerHubImages] = useState<ReactElement[]>([]);
+  const [userRepos, setUserRepos] = useState<ReactElement[]>([]);
+  const [publicRepos, setPublicRepos] = useState<ReactElement[]>([]);
 
   useEffect(() => {
     setShow(true);
   }, []);
 
   useEffect(() => {
-    if (type === "my-github") getUserRepos();
+    if (type === 'my-github') getUserRepos();
   }, [type]);
 
   const getDockerHubImages = async (input: any) => {
     if (!input) return setDockerHubImages([]);
-    // @ts-expect-error TS(2345) FIXME: Argument of type 'Element' is not assignable to pa... Remove this comment to see the full error message
-    setDockerHubImages(<span style={{ color: "#ccc" }}>Loading...</span>);
+    setDockerHubImages([<span style={{ color: '#ccc' }}>Loading...</span>]);
     await fetch(`/api/dockerHubImages/${input}`)
       .then((res) => res.json())
       .then((data) => {
-        const arr = data.map((el: any) => <div
-          key={el.name}
-          className="pod-source"
-          onClick={() => handleClickDockerHub(el.name)}
-        >
+        const arr = data.map((el: any) => (
           <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
+            key={el.name}
+            className="pod-source"
+            onClick={() => handleClickDockerHub(el.name)}
           >
-            <span className="primary">
-              <b>{el.name}</b>
-            </span>
-            <span style={{ fontSize: "12px" }}>
-              {"☆ " + formatStars(el.stars)}
-            </span>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
+            >
+              <span className="primary">
+                <b>{el.name}</b>
+              </span>
+              <span style={{ fontSize: '12px' }}>
+                {'☆ ' + formatStars(el.stars)}
+              </span>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                fontSize: '14px',
+              }}
+            >
+              <span>{el.description}</span>
+            </div>
           </div>
-          <div
-            style={{
-              display: "flex",
-              fontSize: "14px",
-            }}
-          >
-            <span>{el.description}</span>
-          </div>
-        </div>);
+        ));
         setDockerHubImages(arr);
       });
   };
 
   const getUserRepos = async () => {
-    setUserRepos(
-      // @ts-expect-error TS(2345) FIXME: Argument of type 'Element' is not assignable to pa... Remove this comment to see the full error message
-      <div style={{ color: "#ccc", marginTop: "10px" }}>Loading...</div>
-    );
-    await fetch("/api/github/userRepos")
+    setUserRepos([
+      <div style={{ color: '#ccc', marginTop: '10px' }}>Loading...</div>,
+    ]);
+    await fetch('/api/github/userRepos')
       .then((res) => res.json())
       .then((data) => {
-        const arr = data.map((el: any) => <div
-          key={el.name + "-" + el.owner.login}
-          className="pod-source"
-          onClick={() => handleClickGithub(el.full_name)}
-        >
+        const arr = data.map((el: any) => (
           <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
+            key={el.name + '-' + el.owner.login}
+            className="pod-source"
+            onClick={() => handleClickGithub(el.full_name)}
           >
-            <span className="primary">
-              <b>{el.name}</b> by {el.owner.login}
-            </span>
-            <span style={{ fontSize: "12px" }}>
-              {"☆ " + formatStars(el.stargazers_count)}
-            </span>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
+            >
+              <span className="primary">
+                <b>{el.name}</b> by {el.owner.login}
+              </span>
+              <span style={{ fontSize: '12px' }}>
+                {'☆ ' + formatStars(el.stargazers_count)}
+              </span>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                fontSize: '14px',
+              }}
+            >
+              <span>{el.description}</span>
+            </div>
           </div>
-          <div
-            style={{
-              display: "flex",
-              fontSize: "14px",
-            }}
-          >
-            <span>{el.description}</span>
-          </div>
-        </div>);
+        ));
         if (arr.length) setUserRepos(arr);
         else
-          setUserRepos(
-            // @ts-expect-error TS(2345) FIXME: Argument of type 'Element' is not assignable to pa... Remove this comment to see the full error message
-            <div className="tertiary" style={{ marginTop: "10px" }}>
+          setUserRepos([
+            <div className="tertiary" style={{ marginTop: '10px' }}>
               No repositories found.
-            </div>
-          );
+            </div>,
+          ]);
       });
   };
 
   const getPublicRepos = async (input: any) => {
     if (!input) return setPublicRepos([]);
-    // @ts-expect-error TS(2345) FIXME: Argument of type 'Element' is not assignable to pa... Remove this comment to see the full error message
-    setPublicRepos(<span style={{ color: "#ccc" }}>Loading...</span>);
-    await fetch("/api/github/publicRepos", {
-      method: "POST",
+    setPublicRepos([<span style={{ color: '#ccc' }}>Loading...</span>]);
+    await fetch('/api/github/publicRepos', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ input }),
     })
       .then((res) => res.json())
       .then((data) => {
-        const arr = data.items.map((el: any) => <div
-          key={el.name + "-" + el.owner.login}
-          className="pod-source"
-          onClick={() => handleClickGithub(el.full_name)}
-        >
+        const arr = data.items.map((el: any) => (
           <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
+            key={el.name + '-' + el.owner.login}
+            className="pod-source"
+            onClick={() => handleClickGithub(el.full_name)}
           >
-            <span className="primary">
-              <b>{el.name}</b> by {el.owner.login}
-            </span>
-            <span style={{ fontSize: "12px" }}>
-              {"☆ " + formatStars(el.stargazers_count)}
-            </span>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
+            >
+              <span className="primary">
+                <b>{el.name}</b> by {el.owner.login}
+              </span>
+              <span style={{ fontSize: '12px' }}>
+                {'☆ ' + formatStars(el.stargazers_count)}
+              </span>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                fontSize: '14px',
+              }}
+            >
+              <span>{el.description}</span>
+            </div>
           </div>
-          <div
-            style={{
-              display: "flex",
-              fontSize: "14px",
-            }}
-          >
-            <span>{el.description}</span>
-          </div>
-        </div>);
+        ));
         setPublicRepos(arr);
       });
   };
 
   function formatStars(starCount: any) {
     if (starCount < 10 ** 3) return starCount;
-    else if (starCount < 10 ** 6) return (starCount / 1000).toFixed(1) + "k";
-    else return (starCount / 10 ** 9).toFixed(1) + "M";
+    else if (starCount < 10 ** 6) return (starCount / 1000).toFixed(1) + 'k';
+    else return (starCount / 10 ** 9).toFixed(1) + 'M';
   }
 
   const handleClickDockerHub = async (image: any) => {
@@ -170,12 +171,12 @@ export default function () {
         dispatch(
           updateNode({
             id,
-            type: "docker",
+            type: 'docker',
             data: {
               ...data,
-              name: image.split("/").pop(),
+              name: image.split('/').pop(),
               imageName: image,
-              imageTag: imageTags.includes("latest") ? "latest" : imageTags[0],
+              imageTag: imageTags.includes('latest') ? 'latest' : imageTags[0],
               imageTags,
             },
           })
@@ -187,10 +188,10 @@ export default function () {
   };
 
   const handleClickGithub = async (repo: any) => {
-    await fetch("/api/github/branches", {
-      method: "POST",
+    await fetch('/api/github/branches', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ repo }),
     })
@@ -199,13 +200,13 @@ export default function () {
         dispatch(
           updateNode({
             id,
-            type: "github",
+            type: 'github',
             data: {
               ...data,
-              name: repo.split("/").pop(),
+              name: repo.split('/').pop(),
               githubRepo: repo,
-              githubBranch: githubBranches.includes("main")
-                ? "main"
+              githubBranch: githubBranches.includes('main')
+                ? 'main'
                 : githubBranches[0],
               githubBranches,
               imageName: null,
@@ -220,7 +221,7 @@ export default function () {
   };
 
   return (
-    <div className={`modal ${show ? "show" : ""}`}>
+    <div className={`modal ${show ? 'show' : ''}`}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <span className="close-button" onClick={closeModal}>
           &times;
@@ -234,7 +235,7 @@ export default function () {
               <option value="public-github">Public Github</option>
             </select>
           </label>
-          {type === "docker-hub" && (
+          {type === 'docker-hub' && (
             <>
               <label>
                 Search Docker Hub:
@@ -246,28 +247,28 @@ export default function () {
               </label>
               <div
                 style={{
-                  maxHeight: "300px",
-                  overflow: "auto",
+                  maxHeight: '300px',
+                  overflow: 'auto',
                 }}
               >
                 {dockerHubImages}
               </div>
             </>
           )}
-          {type === "my-github" && (
+          {type === 'my-github' && (
             <>
               <label>Select Repository:</label>
               <div
                 style={{
-                  maxHeight: "300px",
-                  overflow: "auto",
+                  maxHeight: '300px',
+                  overflow: 'auto',
                 }}
               >
                 {userRepos}
               </div>
             </>
           )}
-          {type === "public-github" && (
+          {type === 'public-github' && (
             <>
               <label>
                 Search Github:
@@ -279,8 +280,8 @@ export default function () {
               </label>
               <div
                 style={{
-                  maxHeight: "300px",
-                  overflow: "auto",
+                  maxHeight: '300px',
+                  overflow: 'auto',
                 }}
               >
                 {publicRepos}
