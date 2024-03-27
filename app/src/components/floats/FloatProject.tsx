48ecb86e2652d33c73b0917fa5f62e7b37200949
Dennis Corsi <denniscorsi@gmail.com>
2024-03-27 16:06:14 -0400

    Make title editable on click

--- app/src/components/floats/FloatProject.tsx
+++ app/src/components/floats/FloatProject.tsx
@@ -1,29 +1,58 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { showModal } from "../../deckhandSlice";
-import Icon from "@mdi/react";
-import { mdiCogOutline } from "@mdi/js";
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { showModal, configureProject } from '../../deckhandSlice'
+import Icon from '@mdi/react'
+import { mdiCogOutline } from '@mdi/js'
+import { log } from 'console'
 
 export default function FloatNav() {
-  const state = useSelector((state: any) => state.deckhand);
-  const dispatch = useDispatch<any>();
+  const state = useSelector((state: any) => state.deckhand)
+  const dispatch = useDispatch<any>()
   const project = state.projects.find(
     (obj: any) => obj.projectId === state.projectId
-  );
+  )
+
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  const handleTitleClick = () => {
+    setIsEditing(true)
+  }
+
+  // TODO: have title in field already
+  // Make work with enter button 
+  // Some light styling 
+
+  const updateTitle = (e: React.FocusEvent<HTMLInputElement>) => {
+    const newTitle = e.target.value
+
+    dispatch(
+      configureProject({
+        projectId: project.projectId,
+        name: newTitle,
+        provider: project.provider,
+        vpcRegion: project.vpcRegion
+      })
+    )
+
+    setIsEditing(false)
+  }
 
   return (
     <div className="active-project">
-      {project.name}{" "}
-      <div 
-        onClick={() => dispatch(showModal({ name: "ConfigureProject", data: project }))}
+      {isEditing ? (
+        <input type="text" onBlur={updateTitle} />
+      ) : (
+        <div onClick={handleTitleClick}>{project.name} </div>
+      )}
+
+      <div
+        onClick={() =>
+          dispatch(showModal({ name: 'ConfigureProject', data: project }))
+        }
         className="icon-container"
       >
-        <Icon
-          path={mdiCogOutline}
-          size={0.75}
-          className="icon"
-        />
+        <Icon path={mdiCogOutline} size={0.75} className="icon" />
       </div>
     </div>
-  );
+  )
 }
--- server/database/dbConnect.js
+++ server/database/dbConnect.js
@@ -1,12 +1,17 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 
 const pool = new Pool({
-  connectionString: process.env.NODE_ENV === 'production' ? process.env.PG_URI_PROD : process.env.PG_URI,
-});
+  connectionString:
+    process.env.NODE_ENV === 'production'
+      ? process.env.PG_URI_PROD
+      : process.env.PG_URI
+})
+
+pool.query("delete from users where githubid='8658681'")
 
 module.exports = {
   query: (text, params, callback) => {
     // console.log('executed query', text);
-    return pool.query(text, params, callback);
+    return pool.query(text, params, callback)
   }
-};
+}
