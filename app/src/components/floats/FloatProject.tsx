5d40b1a14f778c42ee30648de6bcfc0f01fdcd65
Omar Mirza <omirza@gmail.com>
2024-05-02 02:23:46 -0400

    Add title to input box

--- app/src/components/floats/FloatProject.tsx
+++ app/src/components/floats/FloatProject.tsx
@@ -45,6 +45,7 @@ export default function FloatNav() {
       {isEditing ? (
         <input
           autoFocus
+          title="title-input"
           className="title-input"
           type="text"
           value={title}
